95185e60bbd9b7f8d864ed54b16d17033339992b
Kevin Barralon <me@kbarralon.com>
2018-09-08 22:34:05 -0400

    Test travis

--- build.sh
+++ build.sh
@@ -1,16 +1,19 @@
 #!/usr/bin/env bash
 
+if [ ! -v $NODE_ENV ] && ([ $NODE_ENV == "testing" ] || [ $NODE_ENV == "production" ]); then
+  echo "Building Angular app for $NODE_ENV"
 
-echo "Building Angular app for $NODE_ENV"
-
-build_dev='ng build -c testing'
-if [ $NODE_ENV == "testing" ]; then
- echo "running $build_dev ..."
- eval "$build_dev"
+  build_testing='ng build -c testing'
+  build_prod='ng build -c production'
+  if [ $NODE_ENV == "testing" ]; then
+   echo "running $build_testing ..."
+   eval "$build_testing"
+  elif [ $NODE_ENV == "production" ]; then
+   echo "running $build_prod ..."
+   eval "$build_prod"
+  fi
+else
+  echo "NODE_ENV should be testing or production."
+  exit 1
 fi
 
-build_prod='ng build -c production'
-if [ $NODE_ENV == "production" ]; then
- echo "running $build_prod ..."
- eval "$build_prod"
-fi
