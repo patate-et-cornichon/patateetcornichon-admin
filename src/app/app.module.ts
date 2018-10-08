c07d286cd2c2722ef798d4cf20f973e416e559a3
Kevin Barralon <me@kbarralon.com>
2018-10-08 15:34:39 -0400

    Add french support

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,16 @@
 import { LayoutModule } from '@angular/cdk/layout';
+import { registerLocaleData } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
-import { NgModule } from '@angular/core';
+import localeCaExtra from '@angular/common/locales/extra/fr';
+import localeCa from '@angular/common/locales/fr';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 
+registerLocaleData(localeCa, 'fr-CA', localeCaExtra);
 
 @NgModule({
   declarations: [
@@ -24,7 +28,10 @@ import { CoreModule } from './core/core.module';
   ],
   bootstrap: [
     AppComponent,
-  ]
+  ],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'fr-CA' },
+  ],
 })
 export class AppModule {
 }
