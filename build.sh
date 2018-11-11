f9f59bdf4a05d02a6ae7fb6cb93bc568fb2033fc
Kevin Barralon <me@kbarralon.com>
2018-11-11 01:13:36 -0500

    Updated dependencies

--- build.sh
+++ build.sh
@@ -1,17 +1,8 @@
 #!/usr/bin/env bash
 
 if [ ! -v $NODE_ENV ] && ([ $NODE_ENV == "testing" ] || [ $NODE_ENV == "production" ]); then
-  echo "Building Angular app for $NODE_ENV"
-
-  build_testing='ng build -c testing'
-  build_prod='ng build -c production'
-  if [ $NODE_ENV == "testing" ]; then
-   echo "running $build_testing ..."
-   eval "$build_testing"
-  elif [ $NODE_ENV == "production" ]; then
-   echo "running $build_prod ..."
-   eval "$build_prod"
-  fi
+  echo "Build Angular client on $NODE_ENV ..."
+  ng build -c $NODE_ENV
 else
   echo "NODE_ENV should be testing or production."
   exit 1
